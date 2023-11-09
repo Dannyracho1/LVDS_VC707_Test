466a658d56038b39ae2b297f5405b105ed868c23
Danny Github <64934810+Dannyracho1@users.noreply.github.com>
2023-11-09 10:46:48 +0100

    Vivado constraints (.xdc) added
    Testing vivado bitstream for VC707

---
+++ main2.m
@@ -0,0 +1,94 @@
+
+
+clear
+clc
+close
+
+% filename = "FMC1_Table.xlsx";
+% filename = "FMC2_Table.xlsx";
+filename = "FMC2_Table_No_HB";
+
+[numbers, strings, raw] = xlsread(filename);
+
+
+%% MATLAB generate .UCF constraints --> FMC1
+
+% a = 5;
+% fprintf("Testing %i \n\n\n", a);
+
+% NET "Pin_P" LOC = A0;
+
+for i = 1:1:size(strings,1)
+    str = ['set_property PACKAGE_PIN ' strings{i,2} ' [get_ports ' strings{i,1} ']'];
+    fprintf(str);
+    fprintf('\n');
+end
+
+fprintf('\n\n\n');
+
+% NET "CLKIQ_N[7]" IOSTANDARD = LVDS;
+for i = 1:1:size(strings,1)
+    str = ['set_property IOSTANDARD LVDS [get_ports ' strings{i,1} ']'];
+    fprintf(str);
+    fprintf('\n');
+end
+
+fprintf("\n\n\n ************************** \n\n\n");
+
+
+%% OUTPUT top
+
+for i = 1:1:size(strings,1)
+    if i == size(strings,1)
+        str = ['output ' strings{i,1}];
+    else
+        str = ['output ' strings{i,1} ','];
+    end
+    fprintf(str);
+    fprintf('\n');
+end
+
+
+fprintf("\n\n\n ************************** \n\n\n");
+
+
+%% Differential buffering
+
+%{
+OBUFDS OBUFDS_mod (
+    .I(w_clock), 
+    .O(_P), 	
+    .OB(_N)
+);
+%}
+
+
+for i = 1:2:size(strings,1)
+    str = ['OBUFDS OBUFDS_' strings{i,1} ' (\n\t' ...
+        '.I(w_clock), \n\t' ...
+        '.O(' strings{i+1,1} '), \n\t' ...
+        '.OB(' strings{i,1} ')\n);'];
+    fprintf(str);
+    fprintf('\n');
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
